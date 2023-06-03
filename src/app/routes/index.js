8590dc27ac6a09cf5451701d0a7a3b6295f3bbdf
snow3all <Netithorn_rly@hotmail.com>
2023-06-03 22:02:32 +0700

    [EDIT] Controller name

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,8 +1,8 @@
-const restaurantRoutes = require('../restaurant/route');
+const jenosize = require('../jenosize/route');
 
 module.exports = (app) => {
   app.get('/', (req, res) => {
     res.json({ message: 'API is connected! LOL' })
   })
-  app.use(restaurantRoutes)
+  app.use(jenosize)
 }
